11548bd4db375fae8aba094aa28054c5089c4e29
RemcoVeens <remco@track32.nl>
2026-04-05 13:01:50 +0200

    Fixed test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -7,15 +7,37 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-	t.Fatalf("fix later")
-	want := "pindas"
-	header := http.Header{}
-	header.Set("Authorization", fmt.Sprintf("ApiKey %s", want))
-	res, err := GetAPIKey(header)
-	if res != want {
-		t.Fatalf("expected %s, got %s", want, res)
-	}
-	if err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
+	t.Run("basic good test", func(t *testing.T) {
+		want := "pindas"
+		header := http.Header{}
+		header.Set("Authorization", fmt.Sprintf("ApiKey %s", want))
+		res, err := GetAPIKey(header)
+		if res != want {
+			t.Fatalf("expected %s, got %s", want, res)
+		}
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+	})
+	t.Run("no auth header", func(t *testing.T) {
+		header := http.Header{}
+		res, err := GetAPIKey(header)
+		if err == nil {
+			t.Fatalf("expected error, got nil")
+		}
+		if res != "" {
+			t.Fatalf("expected empty string, got %s", res)
+		}
+	})
+	t.Run("malformed authorization header", func(t *testing.T) {
+		header := http.Header{}
+		header.Set("Authorization", "Bearer pindas")
+		res, err := GetAPIKey(header)
+		if err == nil {
+			t.Fatalf("expected error, got nil")
+		}
+		if res != "" {
+			t.Fatalf("expected empty string, got %s", res)
+		}
+	})
 }
